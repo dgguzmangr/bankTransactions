49b8b1a665bfa4357f778eb7d2a2e2e377314fe7
dgguzmangr <dgguzmangr@gmail.com>
2023-08-08 00:17:53 -0500

    front up to date

---
+++ frontendProject/bankmovements/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1 class="p-3">Mi Aplicación de Movimientos Bancarios</h1>
+    <BankMovements />
+  </div>
+  <!--
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  -->
+</template>
+
+<script>
+import BankMovements from './components/BankMovements.vue';
+
+export default {
+  components: {
+    BankMovements,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ frontendProject/bankmovements/src/components/BankMovements.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <div class="row align-items-center py-3">
+      <div class="col">
+        <h4>Importar Movimientos Bancarios</h4>
+      </div>
+      <div class="col">
+        <input id="excelFile" type="file" @change="uploadFile()" accept=".csv"/>
+      </div>
+    </div>
+    
+    <div class="">
+      <h2>Listado de Movimientos Bancarios</h2>
+      <table v-if="items.length > 0" class="table">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Descripción</th>
+            <th>Moneda</th>
+            <th>Monto</th>
+            <th>Código único</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <td class="text-start" @click="editItem(item, index)">{{ item[0] }}</td>
+            <td class="text-start" @click="editItem(item, index)">{{ item[1] }}</td>
+            <td class="text-start" @click="editItem(item, index)">{{ item[2] }}</td>
+            <td class="text-start" @click="editItem(item, index)">{{ item[3] }}</td>
+            <td class="text-start" @click="editItem(item, index)">{{ item[4] }}</td>
+            
+            <td>
+              <button @click="deleteItem(index)" class="btn btn-danger">Eliminar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button @click="addItem" class="btn btn-success">
+        Agregar Registro
+      </button>
+      <button v-if="items.length > 0" @click="downloadExcelFile()" class="btn btn-primary">
+        Descargar Excel
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Papa from "papaparse";
+import exportXlsFile from "export-from-json";
+
+export default {
+  data() {
+    return {
+      items: [],
+      tipoDeCambio: [
+        { fecha: '2023-05-28', venta: 3.675, compra: 3.671 },
+        { fecha: '2023-05-29', venta: 3.678, compra: 3.674 },
+        { fecha: '2023-05-30', venta: 3.68, compra: 3.667 },
+      ]
+    };
+  },
+  methods: {
+    uploadFile() {
+      const input = document.getElementById("excelFile");
+      const file = input.files[0];
+      
+      if (file) {
+        Papa.parse(file, {
+          complete: (result) => {
+            this.items = result.data;
+          },
+          header: false, // Adjust this if your CSV has headers
+        });
+      }
+    },
+    downloadExcelFile() {
+      const data = this.items;
+      const fileName = 'download';
+      const exportType = exportXlsFile.types.xls;
+      exportXlsFile({ data, fileName, exportType });
+    },
+    addItem() {
+    this.items.push(['', '', '', '', '']); // Agregar un nuevo registro vacío
+    },
+    editItem(item, index) {
+      const newDate = prompt('Editar fecha:', item[0]); // Solicitar nuevo valor
+      if (newDate !== null) {
+        this.items[index][0] = newDate;
+      }
+      const newValue = prompt('Editar descripción:', item[1]); // Solicitar nuevo valor
+      if (newValue !== null) {
+        this.items[index][1] = newValue;
+      }
+      const newMoney = prompt('Editar moneda:', item[2]); // Solicitar nuevo valor
+      if (newMoney !== null) {
+        this.items[index][2] = newMoney;
+      }
+      const newAmount = prompt('Editar monto:', item[3]); // Solicitar nuevo valor
+      if (newAmount !== null) {
+        this.items[index][3] = newAmount;
+      }
+      const newCode = prompt('Editar código único:', item[4]); // Solicitar nuevo valor
+      if (newCode !== null) {
+        this.items[index][4] = newCode;
+      }
+    },
+    deleteItem(index) {
+      if (confirm('¿Seguro que deseas eliminar este registro?')) {
+        this.items.splice(index, 1);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Agrega estilos CSS si es necesario */
+</style>
